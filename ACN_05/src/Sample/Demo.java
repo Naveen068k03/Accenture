911ae8be7b022c44d7de2d71f27305e501088fa8
user <user@DESKTOP-QPVT0GV>
2026-05-19 20:24:12 +0530

    Sample Program

--- ACN_05/src/Sample/Demo.java
+++ ACN_05/src/Sample/Demo.java
@@ -3,8 +3,7 @@ package Sample;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("WelCome Java");
-		System.out.println("WelCome Java");
+		System.out.println("Welcome to Java");
 
 	}
 
