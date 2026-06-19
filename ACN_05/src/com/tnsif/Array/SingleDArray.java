ad48e4604ea60a563ee9c89c6c6510ee14968785
user <user@DESKTOP-QPVT0GV>
2026-06-19 12:07:42 +0530

    Day 9

---
+++ ACN_05/src/com/tnsif/Array/SingleDArray.java
@@ -0,0 +1,21 @@
+package com.tnsif.Array;
+
+public class SingleDArray {
+
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4,5,76,4,7,98};
+	//	int []arr2 = {1,2,34,5};
+	//	int arr3[] = {4,3,5,6};
+		int l = arr.length;
+		System.out.println(1);
+		for(int i = 0; i<1; i++)
+			{
+			System.out.println(arr[i]);
+			}
+	}
+	//------------------------------------------
+	//int[] arr1 = new int[5];
+	//arr1[0] = 101;
+	
+
+}
